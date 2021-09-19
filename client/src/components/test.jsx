05097522ff054a0b3bfa336e4267dabe91ab49b1
Khanh Trinh <khanh.trinh@stunited.vn>
2021-09-19 20:58:35 +0700

    test rebase

--- client/src/components/test.jsx
+++ client/src/components/test.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { withTranslation } from "react-i18next";
 
-const Text = ({ t }) => {
+const Text = ({ t, khanh test }) => {
   return <h2>{t("title")}</h2>;
 };
 
